893e1f23e8e17702ac7d975e9336306af6616936
WangDong <soulw0928@gmail.com>
2020-12-03 18:16:25 +0800

    添加 Oauth And Common 模块

---
+++ liuyun-common/src/main/java/com/liuyun/common/core/config/run/Runner.java
@@ -0,0 +1,34 @@
+package com.liuyun.common.core.config.run;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * runner
+ *
+ * @author wangdong
+ * @version 1.0.0
+ * @date 2020/7/27 15:55
+ **/
+@Slf4j
+@Component
+public class Runner implements ApplicationRunner {
+
+    @Value("${spring.profiles.active}")
+    private String active;
+
+    @Value("${spring.application.name}")
+    private String name;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        log.info("服务: [{}] 环境: [{}] 时间:[{}] 已启动完成!", name, active,
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+    }
+}
